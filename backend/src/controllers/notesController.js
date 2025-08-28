765cb338b4832b4a34f3a0d69de28378b2c1024d
jibin <jibinjolly32@gmail.com>
2025-08-28 10:43:36 +0530

    Mern

---
+++ backend/src/controllers/notesController.js
@@ -0,0 +1,33 @@
+import Note from '../models/Notes.js';
+
+export async function getAllNotes  (_, res)  {
+try {
+    const notes = await Note.find();
+    res.status(200).json(notes);
+}catch (error) {
+    console.error("Error fetching notes:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+}
+
+}
+
+export async function createNote (req, res)  {
+try {
+  const { title, content } = req.body
+  const newNote = new Note({ title, content });
+  
+  await newNote.save();  res.status(201).json({ message: "Note created successfully", note: newNote });  
+
+} catch (error) {
+  console.error("Error creating note:", error);
+  res.status(500).json({ message: "Internal Server Error" }); 
+}
+}               
+
+export function updateNote (req, res)  {
+  res.status(200).json({ message: "Note updated successfully" });
+}
+
+export function deleteNote (req, res)  {
+  res.status(200).json({ message: "Note deleted successfully" });   
+}
